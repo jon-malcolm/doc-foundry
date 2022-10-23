55cc16324ade55d38126fe92ebf0b5a1ec100a27
Jon Malcolm <mail@jonmal.com>
2022-10-23 12:43:05 +0100

    WIP tinkering with types to represent the source folder that will be parsed

--- hello.go
+++ hello.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello, world")
+}
+
+// Scenario 1 - turn a doc path into an indexed tree of documents to start the generation from
+
+func CompileSourceFolder(path string) (DocumentationSourceFolder, error) {
+
+	return DocumentationSourceFolder{}, errors.New("to implement")
+}
+
+type DocumentationSourceFolder struct {
+	Path string
+}
+
+func (d DocumentationSourceFolder) GetPeopleFilePath() string {
+
+	return "todo"
+}
+
+type DocumentationFolder struct {
+	Path         string
+	DocFiles     []DocumentationFile
+	OpenApiFiles []OpenApiFile
+}
+
+func (d DocumentationFolder) GetFolderFilePath() string {
+
+	return "todo"
+}
+
+type DocumentationFile struct {
+	Path string
+}
+
+type OpenApiFile struct {
+	Path string
 }
